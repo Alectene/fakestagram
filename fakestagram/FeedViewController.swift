899210b56294a81cf514c60c38a3c21f3dd69a8f
alectene <alectene@mail.sfsu.edu>
2019-03-11 00:16:18 -0700

    part where I am before error

---
+++ fakestagram/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  fakestagram
+//
+//  Created by Alec Tenefrancia on 3/5/19.
+//  Copyright © 2019 Alec Tenefrancia. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
