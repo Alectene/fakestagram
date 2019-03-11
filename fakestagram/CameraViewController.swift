64d560dab22f1c59558bd196dc6ebde6c7ae7d90
alectene <alectene@mail.sfsu.edu>
2019-03-11 11:55:27 -0700

    user can set picture

--- fakestagram/CameraViewController.swift
+++ fakestagram/CameraViewController.swift
@@ -38,6 +38,17 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate,UI
         present(picker, animated: true, completion: nil)
         
     }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)    }
+    
+    
+    
     
     /*
     // MARK: - Navigation
