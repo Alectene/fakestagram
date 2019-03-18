c7d39dbe33e5efe76fbfcfa2adcb232adc71ba92
alectene <alectene@mail.sfsu.edu>
2019-03-18 11:46:29 -0700

    user can display comments

--- fakestagram/FeedViewController.swift
+++ fakestagram/FeedViewController.swift
@@ -9,11 +9,14 @@
 import UIKit
 import Parse
 import AlamofireImage
+import MessageInputBar
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     
     @IBOutlet weak var tableView: UITableView!
+    let commentBar = MessageInputBar()
+    var showsCommentBar = false
     
     var posts = [PFObject]()
     
@@ -21,15 +24,26 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        
+        tableView.keyboardDismissMode = .interactive
         // Do any additional setup after loading the view.
     }
+    
+    
+    override var inputAccessoryView: UIView?{
+        return commentBar
+    }
+    override var canBecomeFirstResponder: Bool{
+        return showsCommentBar
+        
+    }
+    
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         
         let query = PFQuery(className: "Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments","comments.author"])
         query.limit = 20
         query.findObjectsInBackground{ (posts, error) in
             if posts != nil{
@@ -38,27 +52,49 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             }
         }
     }
-   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        return comments.count + 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")  as! PostCell
         
-        let post = posts[indexPath.row]
-        let user = post["author"] as! PFUser
-        cell.usernameLabel.text = user.username
+         let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
-        cell.captionLabel.text = post["caption"] as! String
-        
-        
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url
-        let url = URL(string: urlString!)!
-        
-        cell.photoView.af_setImage(withURL: url)
-        
-        return cell
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")  as! PostCell
+            
+           
+            let user = post["author"] as! PFUser
+            cell.usernameLabel.text = user.username
+            
+            cell.captionLabel.text = post["caption"] as! String
+            
+            
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url
+            let url = URL(string: urlString!)!
+            
+            cell.photoView.af_setImage(withURL: url)
+            
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            return cell
+        }
+   
     }
     //creates new columns
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
